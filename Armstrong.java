ae7aa67ae5778eee5e581ec01370ce283aafd82f
Atharva Narale <88942727+AtharvaNarale@users.noreply.github.com>
2023-11-11 10:03:52 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,25 @@
+class Armstrong
+{
+	public static void main(String[] args) 
+	{
+		int n=0,arm,r;
+        System.out.println("enter a number");
+		while(n>0)
+		{
+			r=n%10;
+			arm=(r*r*r)+arm;
+			n=n/10;
+		}
+		if(c==arm)
+		int c;
+		{
+			System.out.println("armstrong number");
+		}
+		else
+		{
+			System.out.println("not a armstrong number");
+		}
+
+
+	}
+}
